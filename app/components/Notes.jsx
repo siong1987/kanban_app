460913218e8c9dac52303af17c82e0e7550cf28e
Siong <siong1987@gmail.com>
2015-12-14 23:16:58 -0800

    stage 2.

---
+++ app/components/Notes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Notes from './Notes.jsx';
+
+export default class Notes extends React.Component {
+  render() {
+    <div>
+      <Notes items={notes} />
+    </div>
+  }
+}
